fe8137483b33075c892bb34345f47e9404499fbc
Florian <florian.conti@heig-vd.ch>
2023-01-28 20:02:42 +0100

    dont work for the moment

---
+++ docker/image-auditor/src/index.js
@@ -0,0 +1,69 @@
+import dgram from "dgram"
+import net from "net"
+
+const socket = dgram.createSocket("udp4")
+
+const entries = [
+	["piano", "ti-ta-ti"],
+	["trumpet", "pouet"],
+	["flute", "trulu"],
+	["violin", "gzi-gzi"],
+	["drum", "boum-boum"]
+]
+
+const PROTOCOL = {
+	MULTICAST_ADDRESS: "239.255.22.5",
+	PORT: 9907,
+	TCP_INTERFACE_ADDR: "0.0.0.0",
+	TCP_INTERFACE_PORT: 2205,
+	TIMEOUT: 5000,
+	INSTRUMENTS: new Map([...entries].map(e => e.reverse()))
+}
+
+let musician = new Map();
+
+
+socket.bind(PROTOCOL.PORT, function() {
+	console.log("Joining multicast group");
+	socket.addMembership(PROTOCOL.MULTICAST_ADDRESS);
+});
+
+
+socket.on('message', function(msg, source) {
+
+	const {uuid, sound} = JSON.parse(msg)
+
+	let instrument = PROTOCOL.INSTRUMENTS.get(sound)
+	let date = Date.now()
+
+	musician.set(uuid, {uuid: uuid, instrument: instrument, activeSince: musician.has(uuid) ? musician.get(uuid).activeSince : date, lastActive: date})
+
+	console.log("Data has arrived " + msg + " Source port: " + source.port)
+});
+
+const server = net.createServer()
+
+server.listen(PROTOCOL.TCP_INTERFACE_PORT, () => {
+	console.log("TCP server is running on port " + PROTOCOL.TCP_INTERFACE_PORT)
+}).on('connection', (conn) => {
+	console.log("CONNECTED: " + conn.remoteAddress + ":" + conn.remotePort)
+
+	const date = Date.now()
+	const res = Array.from(musician.entries()).filter(([uuid, musician]) => {
+		let removed = date - musician.lastActive > PROTOCOL.TIMEOUT
+		if(removed) {
+			musician.delete(uuid)
+		}
+		return !removed
+	}).map(([uuid, musician]) => ({
+		uuid,
+		instrument: musician.instrument,
+		activeSince: new Date(musician.activeSince)
+	}))
+
+	conn.write(JSON.stringify(res))
+
+	conn.end()
+}).on('error', (conn) => {
+	console.log;
+});
---
+++ docker/image-musician/src/index.js
@@ -0,0 +1,39 @@
+const entries = [
+	["piano", "ti-ta-ti"],
+	["trumpet", "pouet"],
+	["flute", "trulu"],
+	["violin", "gzi-gzi"],
+	["drum", "boum-boum"]
+]
+
+const PROTOCOL = {
+	MULTICAST_ADDRESS: "239.255.22.5",
+	PORT: 9907,
+	TCP_INTERFACE_ADDR: "0.0.0.0",
+	TCP_INTERFACE_PORT: 2205,
+	TIMEOUT: 5000,
+	INSTRUMENTS: new Map(entries)
+}
+
+import { v4 as uuidv4 } from 'uuid'
+
+import dgram from "dgram"
+
+const s = dgram.createSocket("udp4")
+
+let message = {
+	uuid: uuidv4(),
+	sound: PROTOCOL.INSTRUMENTS.get(process.argv[2])
+};
+
+let payload = JSON.stringify(message);
+
+function sendSound() {
+	s.send(payload, PROTOCOL.PORT, PROTOCOL.MULTICAST_ADDRESS, () => {
+		console.log("Sending payload: " + payload + " via port " + s.address().port);
+	});
+}
+
+sendSound()
+
+setInterval(sendSound, 1000)
